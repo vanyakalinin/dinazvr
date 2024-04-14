1f392467f9d195792dd2f83b5ab25819cb4706a5
vanyakalinin <166290469+vanyakalinin@users.noreply.github.com>
2024-04-14 14:19:22 +0500

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -1,1 +1,27 @@
+from pygame import *
+from random import *
 
+okno = display.set_mode((1200,600))
+ 
+fps = time.Clock() #контроль fps
+game = True
+class gamebo(sprite.Sprite):
+    def __init__(self, img, x,y,w,h):
+        super().__init__()
+        self.image = transform.scale(image.load(img),(w,h))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.lastx = self.rect.x
+        self.lasty = self.rect.y
+    def ris(self):
+        okno.blit(self.image, (self.rect.x, self.rect.y))
+class player(gamebo):
+    def move(self):
+    
+        self.ris()
+        kn = key.get_pressed()
+        if kn[K_LEFT] and self.rect.x > 0:
+            self.rect.x -= 7
+        if kn[K_RIGHT] and self.rect.x < 950:
+            self.rect.x += 7
