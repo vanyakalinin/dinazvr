579a7bef7f5be703edde87bbae6da33d68b738e0
vanyakalinin <166290469+vanyakalinin@users.noreply.github.com>
2024-04-18 23:38:45 +0500

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -7,6 +7,21 @@ fps = time.Clock() #контроль fps
 game = True
 font.init()
 wr = font.Font(None,30)
+class wall(sprite.Sprite):
+    def __init__(self, x,y,w,h):
+        self.image = Surface((w,h))
+        self.image.fill((255,255,255))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def ris(self):
+        okno.blit(self.image, (self.rect.x, self.rect.y))
+
+
+    
+steni = [
+    wall(0,0,1200,10), wall(0,550,1200,10),wall(0,0,10,600),wall(1200,0,10,600)   
+]
 
 class gamebo(sprite.Sprite):
     def __init__(self, img, x,y,w,h):
@@ -37,11 +52,11 @@ class player(gamebo):
             self.rect.y -= 10
         if kn[K_DOWN] and self.rect.y < 600:
             self.rect.y += 10
-farx = 20
-fary = 5
+farx = 5
+fary = 3
 ship = player('стена.png',0,200,50,90)
 ship2 = player2('стена.png',1150,200,50,90)
-ball = player3('мяч.png', 600,300,30,30)
+
 
 class player3(gamebo):
     def move(self):
@@ -50,14 +65,17 @@ class player3(gamebo):
         
         self.rect.x += farx
         self.rect.y += fary
+ball = player3('мяч.png', 600,300,30,30)
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    for w in steni:
+        w.ris()
     
         
-        health -= 10 
+        
         
     okno.fill((0,0,0)) 
     ship.ris()
@@ -66,27 +84,36 @@ while game:
     ship2.move2()
     ball.move()
     if sprite.collide_rect(ball, ship):
-        farx = farx + 2*farx
-        gag = random(1, 3)
+        farx = 5
+        gag = randint(1, 3)
         if gag == 1:
-            fary = fary + 2*fary
+            fary = 0
         if gag == 2:
-            fary = fary + 2 
+            fary = -3
         if gag == 3:
-            fary == fary - 2
-        if sprite.collide_rect(ball, ship2):
-            farx = farx + 2*farx
-        gag = random(1, 3)
+            fary == 3
+    if sprite.collide_rect(ball, ship2):
+        farx = -5
+        gag = randint(1, 3)
         if gag == 1:
-            fary = fary + 2*fary
+            fary = 0
         if gag == 2:
-            fary = fary + 2 
+            fary = -3 
         if gag == 3:
-            fary == fary - 2
+            fary == 3
+    for w in steni:
+            if sprite.collide_rect(ball,w):
+                if ball.rect.y > 300:
+                    fary = -3
+                if ball.rect.y < 300:
+                    fary = 3
+
+               
+                
     
-    pp = wr.render(str(points),False,(200,100,44))
     
-    okno.blit(pp, (1100,80))
+    
+    
     #b.fly()
     
     fps.tick(60)
